1976c380815840109da4fa56e854d26301ad3942
Ian Bissett <ian.bissett@bigcommerce.com>
2016-04-20 12:26:02 +1000

    Move CQL generation into main function and enable better error reporting

--- src/scripts/hubot-confluence-search.js
+++ src/scripts/hubot-confluence-search.js
@@ -24,16 +24,25 @@ var url = require("url");
 
 var username = process.env.HUBOT_CONFLUENCE_USERNAME;
 var password = process.env.HUBOT_CONFLUENCE_PASSWORD;
+var space = process.env.HUBOT_CONFLUENCE_SEARCH_SPACE;
 var host = process.env.HUBOT_CONFLUENCE_HOST;
 
 var uri = url.resolve(host, '/rest/api/content/search');
 
 module.exports = function (robot) {
   robot.confluence_search = new require('./confluence.js')(username, password, host);
-
   //robot.parseHelp(__filename);
   robot.respond(/wiki\s*(.*)$/, function (res) {
-    robot.confluence_search.simpleSearch(res.match[1]).then(function(results) {
+
+    var query = ''
+    if (space) {
+      query = 'space = "' + space + '" and ';
+    }
+    query = query + 'text ~ "' + res.match[1] + '"';
+
+    console.log("Running confluence query: " + query);
+
+    robot.confluence_search.simpleSearch(query).then(function(results) {
       if (results.results.length === 0) {
         res.send( 'Nothing found' );
         return;
